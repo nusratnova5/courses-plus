afe248e79b93feb78806b13885bf0065febea327
nusratnova5 <nusrat.nova16@gmail.com>
2022-09-30 20:28:41 +0600

    card text fix

--- src/components/course/Course.js
+++ src/components/course/Course.js
@@ -13,10 +13,10 @@ function Course(props) {
     <div className='m-3'> 
       <div className='bg-gray-50	p-5 text-left	rounded-2xl shadow-lg'>
         <img src={picture} alt=""></img>
-        <h2 className='font-bold	'>Name: {name}</h2>
-        <p>{newAbout}</p>
+        <h2 className='font-bold'>Name: {name}</h2>
+        <p className='text-gray-500'>{newAbout}</p>
         <p>Time: {time}</p>
-        <p className='text-center'><button className='bg-amber-200	px-16 rounded-lg mt-3' onClick={()=>handleClick(time)}>ADD</button></p>
+        <p className='text-center'><button className='bg-amber-200	px-20 py-1 rounded-lg mt-3' onClick={()=>handleClick(time)}>ADD</button></p>
       </div>
     </div>
   )
